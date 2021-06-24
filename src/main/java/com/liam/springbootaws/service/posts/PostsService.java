d54ad244a400a15c30f73ce2cb83f8bdc1856f2c
dykim <dykim.nexys@gmail.com>
2021-06-24 16:19:02 +0900

    1~3장 예제코드 작성

---
+++ src/main/java/com/liam/springbootaws/service/posts/PostsService.java
@@ -0,0 +1,35 @@
+package com.liam.springbootaws.service.posts;
+
+import com.liam.springbootaws.domain.posts.Posts;
+import com.liam.springbootaws.domain.posts.PostsRepository;
+import com.liam.springbootaws.web.dto.PostsResponseDto;
+import com.liam.springbootaws.web.dto.PostsSaveRequestDto;
+import com.liam.springbootaws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/com/liam/springbootaws/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.liam.springbootaws.web;
+
+import com.liam.springbootaws.service.posts.PostsService;
+import com.liam.springbootaws.web.dto.PostsResponseDto;
+import com.liam.springbootaws.web.dto.PostsSaveRequestDto;
+import com.liam.springbootaws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+}
